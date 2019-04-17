4a9797df5245eec5496cf0333c0ec96a87619702
Konda Yaramala <konda.yaramala@uline.com>
2019-04-17 14:59:49 -0500

    Added source code url for reference

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,8 @@ platformBrowserDynamic().bootstrapModule(AppModule).then(moduleRef => {
   // Also we can run ng.profiler.timeChangeDetection({ record: true});
   // which actually creates a profile in the browser listing all the
   // comps with the time that took for running the change detection
+
+  // for reference https://github.com/angular/angular/blob/master/packages/platform-browser/src/browser/tools/common_tools.ts#L43
   enableDebugTools(componentRef);
 })
 .catch(err => console.log(err));
